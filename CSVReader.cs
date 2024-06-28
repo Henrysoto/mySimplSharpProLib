36ae08cb51151999421824d5468a1e9791bc4a07
Henrysoto <rtcwpub@gmail.com>
2024-06-28 16:16:38 +0200

    init

---
+++ CSVReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Crestron.SimplSharp;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.VisualBasic;
+
+namespace mySimplSharpProLib
+{
+    public class CSVReader
+    {
+        private CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture);
+        
+        public CSVReader()
+        {
+            config.NewLine = Environment.NewLine;
+            config.PrepareHeaderForMatch = args => args.Header.ToLower();
+            config.HasHeaderRecord = true;
+        }
+
+        public void ReadCSV(string filepath)
+        {
+            using (var reader = new StreamReader(filepath))
+            using (var csv = new CsvReader(reader, this.config))
+            {
+                var records = csv.GetRecords<MockType>();
+                foreach (var record in records)
+                {
+                    CrestronConsole.PrintLine($"DEBUG CSV({record.Id}): {record.Email}");
+                }
+            }
+        }
+    }
+}
